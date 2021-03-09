5e477eee6702cae94c28574f02c4f39b27068e31
Cory Johns <johnsca@gmail.com>
2021-03-09 15:14:54 -0500

    Bump version for PyPI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from shutil import rmtree
 
 SETUP = {
     "name": "loadbalancer_interface",
-    "version": "1.0.6",
+    "version": "1.0.7",
     "author": "Cory Johns",
     "author_email": "cory.johns@canonical.com",
     "url": "https://github.com/juju-solutions/loadbalancer-interface",
