3c52935cf39b257a4871889e219e83efd4875c2d
Adam Dyess <adam.dyess@canonical.com>
2022-11-22 16:58:09 -0600

    Update setup.py
    I published an unchanged version of this package by removing some build artifacts from the originally published 1.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from shutil import rmtree
 
 SETUP = {
     "name": "loadbalancer_interface",
-    "version": "1.1.1",
+    "version": "1.1.2",
     "author": "Cory Johns",
     "author_email": "cory.johns@canonical.com",
     "url": "https://github.com/juju-solutions/loadbalancer-interface",
